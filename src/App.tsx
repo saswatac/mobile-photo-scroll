c9edc4b503c0e2124747346aaf23ebf0c09f5daa
Saswata Chakravarty <saswata.chakravarty@neuralconcept.com>
2025-06-10 00:12:16 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+const photos = [
+  {
+    src: "/photos/photo1.jpg",
+    caption: "",
+  },
+  {
+    src: "/photos/photo2.jpg",
+    caption: "Quiet Forest Path",
+  },
+  {
+    src: "/photos/photo3.jpg",
+    caption: "City Skyline at Dusk",
+  },
+];
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen overflow-y-scroll snap-y snap-mandatory">
+      {photos.map((photo, idx) => (
+        <div
+          key={idx}
+          className="h-screen w-screen snap-start flex flex-col"
+        >
+          {/* Image takes up available vertical space except for caption */}
+          <img
+            src={photo.src}
+            alt={photo.caption}
+            className="flex-grow w-full object-cover"
+          />
+
+          {/* Caption is in normal flow, always visible at bottom */}
+          {photo.caption && (
+          <div className="w-full bg-white/80 px-4 py-3 text-center">
+            <h2 className="text-lg text-black font-medium">{photo.caption}</h2>
+          </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
