47c8517630f2ffe3b7f447f52fe23cc8c11c83d2
Saswata Chakravarty <saswata.chakravarty@neuralconcept.com>
2025-06-11 00:49:50 +0530

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationPin, faPhone } from '@fortawesome/free-solid-svg-icons';
+
 const photos = [
   {
     src: "photos/photo1.png",
@@ -15,27 +18,48 @@ const photos = [
 
 export default function App() {
   return (
-    <div className="h-screen w-screen overflow-y-scroll snap-y snap-mandatory">
-      {photos.map((photo, idx) => (
-        <div
-          key={idx}
-          className="h-screen w-screen snap-start flex flex-col"
-        >
-          {/* Image takes up available vertical space except for caption */}
-          <img
-            src={photo.src}
-            alt={photo.caption}
-            className="flex-grow w-full object-contain"
-          />
+    <div className="h-screen w-screen flex flex-col">
+      <div className="flex-1 overflow-y-scroll snap-y snap-mandatory">
+        {photos.map((photo, idx) => (
+          <div
+            key={idx}
+            className="h-screen w-screen snap-start flex flex-col"
+          >
+            {/* Image takes up available vertical space except for caption */}
+            <img
+              src={photo.src}
+              alt={photo.caption}
+              className="flex-grow w-full object-contain"
+            />
 
-          {/* Caption is in normal flow, always visible at bottom */}
-          {photo.caption && (
-          <div className="w-full bg-white/80 px-4 py-3 text-center">
-            <h2 className="text-lg text-black font-medium">{photo.caption}</h2>
+            {/* Caption is in normal flow, always visible at bottom */}
+            {photo.caption && (
+              <div className="w-full bg-white/80 px-4 py-3 text-center">
+                <h2 className="text-lg text-black font-medium">{photo.caption}</h2>
+              </div>
+            )}
           </div>
-          )}
-        </div>
-      ))}
+        ))}
+      </div>
+      {/* Footer */}
+      <footer className="w-full bg-gray-900 text-white py-4 px-6 flex flex-row items-center justify-between">
+        <span className="flex items-center gap-2">
+          <FontAwesomeIcon icon={faPhone} />
+          <a href="tel:+918240596697" className="underline">8240596697</a>
+        </span>
+        <span className="flex items-center gap-2">
+          <FontAwesomeIcon icon={faLocationPin} className="mr-2" />
+          <a
+            href="https://maps.app.goo.gl/PQYx8eLcVFYWyeBz6"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+            aria-label="View Location"
+          >
+            SonarTari
+          </a>
+        </span>
+      </footer>
     </div>
   );
 }
